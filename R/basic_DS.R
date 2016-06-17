4ce46d6bac7956717cf2eba32a28e619790538d4
emeyers <emeyers@hampshire.edu>
2016-06-17 17:50:53 -0400

    Updated basic_DS.R so that it you can repeat labels in each CV block using the num.times.to.repeat.labels.per.cv.block property

--- R/basic_DS.R
+++ R/basic_DS.R
@@ -19,6 +19,7 @@ basic_DS <- R6Class("basic_DS",
     specific.binned.label.name = NA, 
     num.cv.splits = NA,
     use.count.data = FALSE,
+    num.times.to.repeat.labels.per.cv.block = 1,
   
     
     # constructor
@@ -48,8 +49,7 @@ basic_DS <- R6Class("basic_DS",
       # (at the cost of a little memory)
       binned.data <- self$binned.data 
       specific.binned.label.name <- self$specific.binned.label.name
-      num.cv.splits = self$num.cv.splits
-      
+      num.trials.used.per.label <- self$num.cv.splits * self$num.times.to.repeat.labels.per.cv.block 
     
   
       # remove all labels that aren't being used, and rename the labels that are being used "labels"
@@ -59,7 +59,7 @@ basic_DS <- R6Class("basic_DS",
       
       
       # order data by: repetitions, sites, labels
-      all.k.fold.data <- binned.data  %>% group_by(labels, siteID) %>% sample_n(size = num.cv.splits)
+      all.k.fold.data <- binned.data  %>% group_by(labels, siteID) %>% sample_n(size = num.trials.used.per.label)
       
       
       unique.labels <- unique(all.k.fold.data$labels)
@@ -68,38 +68,48 @@ basic_DS <- R6Class("basic_DS",
       num.labels <- length(unique.labels)
       
       
-      # modify a few names in the data
-      CV.num <- rep(1:num.cv.splits, num.labels * num.sites)
+      # add a few names in the data frame
+      
+      # CV.slice.ID is a groups of data that have one example for each label
+      #  - these groups are mapped into CV blocks where blocks contain num.times.to.repeat.labels.per.cv.block of each label  
+      CV.slice.ID <- rep(1:num.trials.used.per.label, num.labels * num.sites)
       
       # add the number of the cross-validitation split to the data ...
-      all.k.fold.data2 <- all.k.fold.data
-      all.k.fold.data2$CV.num <- CV.num
+      all.k.fold.data$CV.slice.ID <- CV.slice.ID
       
       # paste the site.000 in front of the siteID so that is is listed as site.0001, site.0002, etc
-      all.k.fold.data2$siteID <- paste0("site.", str_pad(all.k.fold.data2$siteID, 4, pad = "0"))
+      all.k.fold.data$siteID <- paste0("site.", str_pad(all.k.fold.data$siteID, 4, pad = "0"))
 
-      
       
       # reshape the data so that it's [label*time*cv x site]  data frame 
       # can do this quickly using the reshape2 package!
       
-      melted.data <- melt(all.k.fold.data2, id.vars = c("siteID", "labels", "CV.num"), 
+      melted.data <- melt(all.k.fold.data, id.vars = c("siteID", "labels", "CV.slice.ID"), 
                           variable.name = "time", value.name = "activity")
       
-      all.cv.data <- dcast(melted.data, labels + time + CV.num ~ siteID, value.var = "activity")
+      all.cv.data <- dcast(melted.data, labels + time + CV.slice.ID ~ siteID, value.var = "activity")
       
 
 
-      # should change this so can put repeats of labels in a CV split...
+      # below I changed this so can put repeats of labels in a CV split...
+      # # create different CV.1, CV.2 which list which points are training points and which points are test points
+      # for (iCV in 1:num.cv.splits) {
+      #   eval(parse(text=paste0("all.cv.data$CV.", iCV, "= ifelse(all.cv.data$CV.slice.ID == iCV, 'test', 'train')")))
+      # }
+      # all.cv.data <- select(all.cv.data, -CV.slice.ID)  # remove the original CV.slice.ID field     
+
+      
       
       # create different CV.1, CV.2 which list which points are training points and which points are test points
-      for (iCV in 1:num.cv.splits) {
-        eval(parse(text=paste0("all.cv.data$CV.", iCV, "= ifelse(all.cv.data$CV.num == iCV, 'test', 'train')")))
+      for (iCV in 1:self$num.cv.splits) {
+        start.ind <- (((iCV - 1) * self$num.times.to.repeat.labels.per.cv.block) + 1)
+        end.ind <- (iCV * self$num.times.to.repeat.labels.per.cv.block)
+        curr.cv.block.inds <- start.ind:end.ind
+        eval(parse(text=paste0("all.cv.data$CV.", iCV, "= ifelse(all.cv.data$CV.slice.ID %in% curr.cv.block.inds, 'test', 'train')")))
       }
-      all.cv.data <- select(all.cv.data, -CV.num)  # remove the original CV.num field     
-
+      all.cv.data <- select(all.cv.data, -CV.slice.ID)  # remove the original CV.slice.ID field     
       
-
+      
       return(all.cv.data)
       
       
