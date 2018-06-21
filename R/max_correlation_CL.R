5c6d3f7367b28879ad5adfba516d29ce34afdfe8
Lily Rithichoo <rithi22s@mtholyoke.edu>
2018-06-21 14:58:42 -0400

    indentation

--- R/max_correlation_CL.R
+++ R/max_correlation_CL.R
@@ -27,72 +27,39 @@
 
 
 max_correlation_CL <- R6Class("max_correlation_CL", 
-                       
-   public = list(
-                         
-     # no properties for this classifier
-     
-
+  public = list(
+    # no properties for this classifier
+    
     # the constructor does not take any arguments
     initialize = function() {},
                          
-    
     # methods
-    
-    
     # could break this up into two methods: train() and test()
     get_predictions = function(train_data, all_times_test_data) {    
-  
-
-      
       ### Train the classifier
-      
       prototypes <- train_data %>% group_by(labels) %>% summarise_each(funs(mean))
-      
-      
-      
-      
       ### Test the classifier
-      
-      
       train_test_cor <- cor(t(prototypes[, 2:dim(prototypes)[2]]), t(select(all_times_test_data, -labels, -time)))
       #train_test_cor <- cor(t(prototypes[, 2:133]), t(select(all_times_test_data, -labels, -time)))
-      
 
       # get the predicted labels
-      
       # predicted_inds <- apply(train_test_cor, 2, which.max)   # need to create rand.which.max() function...
       #predicted_inds <- apply(train_test_cor, 2, which.is.max)   # only slightly slower but breaks ties
       predicted_inds <- apply(train_test_cor, 2, rand_which_max)   # only slightly slower but breaks ties
-      
-      
       predicted_labels <- prototypes$labels[predicted_inds]
-      
-      
       # create a data frame that has all the results
       results <- data.frame(time = all_times_test_data$time, actual_labels = all_times_test_data$labels, 
                             predicted_labels = predicted_labels) %>%
         mutate(correct = actual_labels == predicted_labels)
-      
-      
       # get the decision values
       decision_values <- data.frame(t(train_test_cor))
       names(decision_values) <- paste0('decision_val_', prototypes$labels)  
-      
-      
       # return the results
       results <- cbind(results, decision_values)
+      
       return(results)
-      
-    
-      
-      
       }   # end the get_predictions method
-    
-    
    )
-   
-   
 )  # end the class
 
 
