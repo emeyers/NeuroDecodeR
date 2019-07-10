7f218af42ceed45351f88c95a83bf005c80b1c78
emeyers <emeyers@hampshire.edu>
2019-07-09 20:40:58 -0400

    Added a file NDTr.R which just will contain documentation about the package as a whole.

---
+++ R/NDTr.R
@@ -0,0 +1,39 @@
+#' NDTr: A package for doing neural decoding
+#'
+#' The NDTr is a package that makes it easy to do neural decoding analyses.
+#' 
+#' The NDTr is built around 5 abstract object types that work together in a
+#' modular way to allow a range of neural decoding analyses. These five object
+#' types are:
+#' 
+#'@section 1. Datasources:
+#'  Datasources take
+#' 
+#'@section 2. Feature preprofessors:
+#'  XXX
+#' 
+#'@section 3. Classifiers:
+#'  XXX
+#'
+#'@section 4. Result metrics: 
+#'  XXX
+#'
+#'@section 5. Cross-validators:
+#'  XXX
+#'  
+#'@section Data formats: 
+#'  XXX
+#'
+#'@docType package
+#'@name NDTr
+#'
+#'
+
+
+NULL
+
+
+
+
+
+
