4512b0195ba5c508b56d407760b63ca472d96d24
Ethan Meyers <ethan.meyers@gmail.com>
2020-07-15 13:06:59 -0400

    Updated the FPS a litte bit using styler and lintr

--- R/fp_select_k_features.R
+++ R/fp_select_k_features.R
@@ -9,41 +9,42 @@
 #' performance using the most selective 10 neurons to using the next 10 most
 #' selective neurons, etc.).
 #'
-#' @param num_site_to_use The number of features with the smallest p-values to use. 
+#' @param num_site_to_use The number of features with the smallest p-values to use.
 #'
 #' @param num_sites_to_exclude The number of features with the smallest p-values
 #'   that should be excluded.
-#' 
+#'
 #' @examples
 #' # This will cause the cross-validator use only the 50 most selective sites
 #' fp <- fp_select_k_features(num_site_to_use = 50)
-#' 
+#'
 #' # This will cause the cross-validator to remove the 20 most selective sites
 #' fp <- fp_select_k_features(num_sites_to_exclude = 20)
 #'
-#' # This will cause the cross-validator to remove the 20 most selective sites 
-#' # and then use only the 50 most selective sites that remain after the 20 are 
-#' # eliminated 
+#' # This will cause the cross-validator to remove the 20 most selective sites
+#' # and then use only the 50 most selective sites that remain after the 20 are
+#' # eliminated
 #' fp <- fp_select_k_features(50, 20)
-#' 
-#'
 #' @family feature_preprocessor
-#' 
-#' 
-# the constructor 
+#'
+#'
+# the constructor
 #' @export
 fp_select_k_features <- function(num_site_to_use = NA,
-                                 num_sites_to_exclude = NA){
-  
+                                 num_sites_to_exclude = NA) {
   if (is.na(num_site_to_use) && is.na(num_sites_to_exclude)) {
-    stop(paste0('Either num_site_to_use or num_sites_to_exclude must be set',
-                'prior to calling the preprocess_data method'))
+    stop(paste0(
+      "Either num_site_to_use or num_sites_to_exclude must be set",
+      "prior to calling the preprocess_data method"
+    ))
   }
-  
-  
-  the_fp <- list(num_site_to_use = num_site_to_use,
-                 num_sites_to_exclude = num_sites_to_exclude)
-  
+
+
+  the_fp <- list(
+    num_site_to_use = num_site_to_use,
+    num_sites_to_exclude = num_sites_to_exclude
+  )
+
   attr(the_fp, "class") <- "fp_select_k_features"
   the_fp
 }
@@ -53,80 +54,86 @@ fp_select_k_features <- function(num_site_to_use = NA,
 
 
 #' @export
-preprocess_data.fp_select_k_features = function(fp, training_set, test_set){
+preprocess_data.fp_select_k_features <- function(fp, training_set, test_set) {
 
 
-# writing the ANOVA function myself so that it runs faster
+  # writing the ANOVA function myself so that it runs faster
 
-# get the ANOVA p-values for all sites...   
-num_points_in_each_group <- training_set %>% group_by(.data$train_labels) %>% summarize(n = n())
-num_sites <- dim(training_set)[2] - 1
-num_groups <- dim(num_points_in_each_group)[1]    # the number of classes
+  # get the ANOVA p-values for all sites...
+  num_points_in_each_group <- training_set %>%
+    group_by(.data$train_labels) %>%
+    summarize(n = n())
+  num_sites <- dim(training_set)[2] - 1
+  num_groups <- dim(num_points_in_each_group)[1] # the number of classes
 
-# marginally faster way to compute the group means (might have more of a speed up if more sites are used)
-split_data <- split(training_set[, 1:num_sites], training_set$train_labels)
-group_means <- t(sapply(split_data, function(one_group_data) apply(one_group_data, 2, mean)))
+  # marginally faster way to compute the group means
+  split_data <- split(training_set[, 1:num_sites], training_set$train_labels)
+  group_means <- t(sapply(split_data, function(one_group_data) apply(one_group_data, 2, mean)))
 
-MSS <- apply(sweep(scale(group_means, scale = FALSE)^2, 1, num_points_in_each_group$n, FUN = "*"), 2, sum)
-TSS <- apply(scale(select(training_set, -.data$train_labels), scale = FALSE)^2, 2, sum)
-SSE <- TSS - MSS   # residual SS = total SS + model SS
+  MSS <- apply(sweep(scale(group_means, scale = FALSE)^2, 1,
+                     num_points_in_each_group$n, FUN = "*"), 2, sum)
+  TSS <- apply(scale(select(training_set, -.data$train_labels), scale = FALSE)^2, 2, sum)
+  SSE <- TSS - MSS # residual SS = total SS + model SS
 
-between_deg_free <- num_groups - 1
-within_deg_free <- dim(training_set)[1] - num_groups
+  between_deg_free <- num_groups - 1
+  within_deg_free <- dim(training_set)[1] - num_groups
 
-f_stats <- (MSS/between_deg_free)/(SSE/within_deg_free)
-all_pvals <- pf(f_stats, df1 = between_deg_free, df2 = within_deg_free, lower.tail = FALSE)
+  f_stats <- (MSS / between_deg_free) / (SSE / within_deg_free)
+  all_pvals <- pf(f_stats, df1 = between_deg_free, df2 = within_deg_free, 
+                  lower.tail = FALSE)
 
-# find the sites with the k smallest p-values
-sorted_data <- sort(all_pvals, index.return = TRUE)
-sites_to_use <- sorted_data$ix
+  # find the sites with the k smallest p-values
+  sorted_data <- sort(all_pvals, index.return = TRUE)
+  sites_to_use <- sorted_data$ix
 
 
-# if excluding selective sites, first remove these num_sites_to_exclude sites 
-if (!is.na(fp$num_sites_to_exclude)) {
-  sites_to_use <- sites_to_use[(fp$num_sites_to_exclude  + 1):num_sites]
+  # if excluding selective sites, first remove these num_sites_to_exclude sites
+  if (!is.na(fp$num_sites_to_exclude)) {
+    sites_to_use <- sites_to_use[(fp$num_sites_to_exclude + 1):num_sites]
+  }
+
+  # use only the num_site_to_use most selective sites
+  if (!is.na(fp$num_site_to_use)) {
+    sites_to_use <- sites_to_use[1:fp$num_site_to_use]
+  }
+
+
+
+  # Returning information about which sites were selected. This could 
+  #  be saved by the cross-validator to find out which sites were used.
+  selected_site <- rep(FALSE, dim(select(training_set, starts_with("site")))[2])
+  selected_site[sites_to_use] <- TRUE
+  siteID <- as.numeric(substr(names(select(training_set, starts_with("site"))), 6, 100))
+  fp_info <- data.frame(
+    siteID = siteID,
+    pvals = all_pvals,
+    selected_site = selected_site
+  )
+
+
+  # return a list with the preprocessed data
+  processed_data <- list(
+    training_set = cbind(training_set[sites_to_use],
+      train_labels = training_set$train_labels
+    ),
+    test_set = cbind(test_set[sites_to_use],
+      test_labels = test_set$test_labels,
+      time_bin = test_set$time_bin
+    ),
+    fp_info = fp_info
+  )
+
+  processed_data
 }
-
-# use only the num_site_to_use most selective sites
-if (!is.na(fp$num_site_to_use)) {
-  sites_to_use <- sites_to_use[1:fp$num_site_to_use]
-}
-
-
-
-# returning information about which sites were selected
-#  this could be saved by the cross-validator to find out which sites were used
-selected_site <- rep(FALSE, dim(select(training_set, starts_with("site")))[2])
-selected_site[sites_to_use] <- TRUE
-siteID <- as.numeric(substr(names(select(training_set, starts_with("site"))), 6, 100))
-fp_info <- data.frame(siteID = siteID,
-                            pvals = all_pvals, 
-                            selected_site = selected_site)
-
-
- # return a list with the preprocessed data
- processed_data <- list(training_set = cbind(training_set[sites_to_use], 
-                                             train_labels = training_set$train_labels), 
-                        test_set = cbind(test_set[sites_to_use], test_labels = test_set$test_labels, 
-                                         time_bin = test_set$time_bin), 
-                        fp_info = fp_info)
-    
- processed_data
- 
-}  
 
 
 
 
 
 #' @export
-get_parameters.fp_select_k_features = function(ndtr_obj) {
-  
-    data.frame(fp_select_k_features.num_site_to_use = ndtr_obj$num_site_to_use,
-             fp_select_k_features.num_sites_to_exclude = ndtr_obj$num_sites_to_exclude)
+get_parameters.fp_select_k_features <- function(ndtr_obj) {
+  data.frame(
+    fp_select_k_features.num_site_to_use = ndtr_obj$num_site_to_use,
+    fp_select_k_features.num_sites_to_exclude = ndtr_obj$num_sites_to_exclude
+  )
 }
-
-
-
-
-
--- R/fp_zscore.R
+++ R/fp_zscore.R
@@ -14,18 +14,16 @@
 #' different ranges of values (for example, it is useful when decoding neural
 #' data because different neurons can have different ranges of firing rates).
 #'
-#'@examples 
+#' @examples
 #' # The fp_zscore() constructor does not take any parameters. This object
-#' # just needs to added to a list and passed to the cross-validator applied 
+#' # just needs to added to a list and passed to the cross-validator applied
 #' fp <- fp_zscore()
-#'
-#'
 #' @family feature_preprocessor
-#' 
-#' 
-# the constructor 
+#'
+#'
+#'
 #' @export
-fp_zscore <- function(){
+fp_zscore <- function() {
   the_fp <- list()
   attr(the_fp, "class") <- "fp_zscore"
   the_fp
@@ -34,42 +32,42 @@ fp_zscore <- function(){
 
 
 #' @export
-preprocess_data.fp_zscore = function(fp, training_set, test_set){
-  
+preprocess_data.fp_zscore <- function(fp, training_set, test_set) {
+
   # separate the data from the labels
-  train_labels <- select(training_set, -starts_with('site'))
-  train_data <- select(training_set, starts_with('site'))
-  
-  test_labels <- select(test_set, -starts_with('site'))
-  test_data <- select(test_set, starts_with('site'))
-  
+  train_labels <- select(training_set, -starts_with("site"))
+  train_data <- select(training_set, starts_with("site"))
+
+  test_labels <- select(test_set, -starts_with("site"))
+  test_data <- select(test_set, starts_with("site"))
+
 
   # get the parameters and cale the rianing data (all in one line!)
   train_zscored <- scale(train_data, center = TRUE, scale = TRUE)
-  
-  
+
+
   # apply the parameters from trianing data to scale the test data
-  # test_centered <- sweep(test_data, 2, attr(train_zscored, "scaled:center") )
-  # test_zscored <- sweep(test_centered, 2, attr(train_zscored, "scaled:scale"), FUN = "/")
-  
-  # this is much faster
-  test_zscored <- scale(test_data, center = attr(train_zscored, "scaled:center"), 
-                scale = attr(train_zscored, "scaled:scale"))
-  
-  
-  # set to 0 all InF and NAs that could occur due to the standard deviation of a site being 0
+  #  (much faster way to z-score the data compared to using the sweep function)
+  test_zscored <- scale(test_data,
+    center = attr(train_zscored, "scaled:center"),
+    scale = attr(train_zscored, "scaled:scale")
+  )
+
+
+  # set to 0 all InF and NAs that occur due to the sd of a site being 0
   train_zscored[is.na(train_zscored)] <- 0
   test_zscored[is.na(test_zscored)] <- 0
-  train_zscored[is.infinite(train_zscored)] <- 0     # perhaps should deal with the Infs differently...
+  train_zscored[is.infinite(train_zscored)] <- 0 # could deal with the Infs differently...
   test_zscored[is.infinite(as.matrix(test_zscored))] <- 0
-  
+
   # return a list with the results (after adding the labels back to the data)
-  processed_data <- list(training_set = cbind(train_zscored, train_labels), 
-                         test_set = cbind(test_zscored, test_labels))
-  
+  processed_data <- list(
+    training_set = cbind(train_zscored, train_labels),
+    test_set = cbind(test_zscored, test_labels)
+  )
+
   return(processed_data)
-  
-} 
+}
 
 
 
@@ -77,9 +75,6 @@ preprocess_data.fp_zscore = function(fp, training_set, test_set){
 # since there are no parameters for the fp_zscore just return a data frame with
 # fp_zscore.fp_zscore and a value of "No parameters"
 #' @export
-get_parameters.fp_zscore = function(ndtr_obj) {
+get_parameters.fp_zscore <- function(ndtr_obj) {
   data.frame(fp_zscore.fp_zscore = "does not have settable parameters")
 }
-
-
-
